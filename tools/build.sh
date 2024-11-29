432cfc479e55cac13c12fe0e647531c2a114e1d3
T-Honda-dev <hazer231@yahoo.co.jp>
2024-11-29 22:05:58 +0900

    resource-pathを追加

--- tools/build.sh
+++ tools/build.sh
@@ -50,10 +50,12 @@ dest_dir="$(dirname "$dest")"
 mkdir -p "$dest_dir"
 
 css_rel_path="$("$realpath" "docs/css/" --relative-to "$dest_dir")"
+img_folder="$("$realpath" "docs/img/" --relative-to "$dest_dir")"
 
 pandoc \
   --katex \
   --from markdown+tex_math_single_backslash \
+  --resource-path $img_folder \
   --filter pandoc-sidenote.js \
   --to html5+smart \
   --template=template \
